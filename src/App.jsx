f6685bcef97c6312d17878467df592982657c3e1
kachakaran6 <kachakaran06@gmail.com>
2025-03-17 21:06:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-500 bg-black py-2 px-3 text-center">
+        Hello World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
